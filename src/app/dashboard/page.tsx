cb71965a41930ee35ae4e32f316db1c1b44e061f
Atharva Kharage <atharvak1520@gmail.com>
2024-09-03 21:23:37 -0700

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Button from '@/components/ui/Button'
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return (
+    <Button variant='default'>Default</Button>
+  )
+}
+
+export default page
