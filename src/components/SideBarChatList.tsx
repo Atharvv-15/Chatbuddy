7e3d5053575d6c2e2aecf91763324463a23c3a0a
Atharva Kharage <atharvak1520@gmail.com>
2024-09-10 22:22:04 -0700

    fixed minor bugs

--- src/components/SideBarChatList.tsx
+++ src/components/SideBarChatList.tsx
@@ -26,13 +26,14 @@ const SideBarChatList: FC<SideBarChatListProps> = ({friends, sessionId}) => {
     const pathname = usePathname()
 
     const [unseenMessages, setUnseenMessages] = useState<Message[]>([])
+    const [activeChats, setActiveChats] = useState<User[]>(friends)
 
     useEffect(() => {
         pusherClient.subscribe(toPusherKey(`user:${sessionId}:chats`))
         pusherClient.subscribe(toPusherKey(`user:${sessionId}:friends`))
 
-        const newFriendHandler = () => {
-            router.refresh()
+        const newFriendHandler = (newFriend: User) => {
+            setActiveChats((prev) => [...prev, newFriend])
         }
 
         const chatHandler = (message: ExtendedMessage) => {
